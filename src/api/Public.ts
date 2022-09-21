d778ba15d2fd2e3b3bcd098f1e376996aa952cbb
刘雪可 <1097678164@qq.com>
2022-09-21 15:12:37 +0800

    Update Public.ts

--- src/api/Public.ts
+++ src/api/Public.ts
@@ -1,5 +1,18 @@
 import request from '@/utils/request.ts';
+import qs from 'qs'
 
-export function ownMenu(){
+export function getMenu(){
     return request.get("/api/menu")
+}
+
+export function getList(params){
+    return request.get(
+        "/api/list",
+        {
+            params,
+            paramsSerializer:params=>{
+                return qs.stringify(params,{arrayFormat:'repeat'})
+            }
+        }
+    )
 }
